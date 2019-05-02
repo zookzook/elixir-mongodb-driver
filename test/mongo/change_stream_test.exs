c52cd5daf9eaab3e97dc041feb9993d42e57bfb4
Michael Maier <zookzook@unitybox.de>
2019-05-02 21:11:13 +0200

    debugging travis: fixing timing problems

--- test/mongo/change_stream_test.exs
+++ test/mongo/change_stream_test.exs
@@ -8,18 +8,21 @@ defmodule Mongo.ChangeStreamTest do
   end
 
   def consumer_1(top, monitor) do
+    Process.sleep(1000)
     cursor = Mongo.watch_collection(top, "users", [], fn doc -> IO.puts("Token #{inspect doc}"); send(monitor, {:token, doc}) end, max_time: 1_000, debug: true )
     result = cursor |> Enum.take(2) |> Enum.at(0)
     send(monitor, {:insert, result})
   end
 
   def consumer_2(top, monitor, token) do
+    Process.sleep(1000)
     cursor = Mongo.watch_collection(top, "users", [], fn doc -> IO.puts("Token #{inspect doc}"); send(monitor, {:token, doc}) end, resume_after: token, max_time: 1_000 )
     result = cursor |> Enum.take(1) |> Enum.at(0)
     send(monitor, {:insert, result})
   end
 
   def consumer_3(top, monitor, token) do
+    Process.sleep(1000)
     cursor = Mongo.watch_collection(top, "users", [], fn doc -> IO.puts("Token #{inspect doc}"); send(monitor, {:token, doc}) end, resume_after: token, max_time: 1_000 )
     result = cursor |> Enum.take(4) |> Enum.map(fn %{"fullDocument" => %{"name" => name}} -> name end)
     send(monitor, {:insert, result})
@@ -27,7 +30,7 @@ defmodule Mongo.ChangeStreamTest do
   end
 
   def producer(top) do
-    Process.sleep(300)
+    Process.sleep(2000)
     assert {:ok, %Mongo.InsertOneResult{}} = Mongo.insert_one(top, "users", %{name: "Greta"})
     assert {:ok, %Mongo.InsertOneResult{}} = Mongo.insert_one(top, "users", %{name: "Gustav"})
     assert {:ok, %Mongo.InsertOneResult{}} = Mongo.insert_one(top, "users", %{name: "Tom"})
@@ -40,9 +43,9 @@ defmodule Mongo.ChangeStreamTest do
     spawn(fn -> consumer_1(top, me) end)
     spawn(fn -> producer(top) end)
 
-    assert_receive {:token, nil}, 2_000
-    assert_receive {:token, token}, 2_000
-    assert_receive {:insert, %{"fullDocument" => %{"name" => "Greta"}}}, 2_000
+    assert_receive {:token, nil}, 5_000
+    assert_receive {:token, token}, 5_000
+    assert_receive {:insert, %{"fullDocument" => %{"name" => "Greta"}}}, 5_000
 
     Process.sleep(500)
 
@@ -51,16 +54,18 @@ defmodule Mongo.ChangeStreamTest do
     spawn(fn -> consumer_2(top, me, token) end)
     spawn(fn -> producer(top) end)
 
-    assert_receive {:token, _}, 2_000
-    assert_receive {:insert, %{"fullDocument" => %{"name" => "Gustav"}}}, 2_000
+    assert_receive {:token, _}, 5_000
+    assert_receive {:insert, %{"fullDocument" => %{"name" => "Gustav"}}}, 5_000
 
     Process.sleep(500)
 
     spawn(fn -> consumer_3(top, me, token) end)
     spawn(fn -> producer(top) end)
 
-    assert_receive {:token, _}, 2_000
-    assert_receive {:insert, ["Gustav", "Tom", "Liese", "Greta"]}, 2_000
+    assert_receive {:token, _}, 5_000
+    assert_receive {:insert, ["Gustav", "Tom", "Liese", "Greta"]}, 5_000
+
+    Process.sleep(1000)
 
   end
 end
