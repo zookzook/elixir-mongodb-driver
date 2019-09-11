41762489829c025fc5027a9b32facbd71a2b9b78
Michael Maier <zookzook@unitybox.de>
2019-09-11 16:37:40 +0200

    added test for streaming bulk operation with transaction

--- test/mongo/session_test.exs
+++ test/mongo/session_test.exs
@@ -9,27 +9,28 @@ defmodule Mongo.SessionTest do
   alias Mongo.OrderedBulk
   alias Mongo.BulkWrite
   alias Mongo.BulkWriteResult
+  alias Mongo.BulkOps
 
   setup_all do
-    assert {:ok, pid} = Mongo.TestConnection.connect
-    {:ok, [pid: pid]}
+    assert {:ok, top} = Mongo.TestConnection.connect
+    {:ok, %{top: top}}
   end
 
   @tag :mongo_3_6
-  test "simple session insert", top do
+  test "simple session insert", %{top: top} do
     coll = unique_name()
 
-    {:ok, session} = Session.start_session(top.pid, :write, [])
-    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Greta"}, session: session)
+    {:ok, session} = Session.start_session(top, :write, [])
+    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Greta"}, session: session)
     assert id != nil
-    assert :ok == Session.end_session(top.pid, session)
+    assert :ok == Session.end_session(top, session)
 
   end
 
   @tag :mongo_3_6
-  test "session pool fifo", top do
+  test "session pool fifo", %{top: top} do
 
-    {:ok, session_pool} = SessionPool.start_link(top.pid, 30)
+    {:ok, session_pool} = SessionPool.start_link(top, 30)
 
     session_a = SessionPool.checkout(session_pool)
     session_b = SessionPool.checkout(session_pool)
@@ -42,9 +43,9 @@ defmodule Mongo.SessionTest do
   end
 
   @tag :mongo_3_6
-  test "session pool checkin prune", top do
+  test "session pool checkin prune", %{top: top} do
 
-    {:ok, session_pool} = SessionPool.start_link(top.pid, 1)
+    {:ok, session_pool} = SessionPool.start_link(top, 1)
 
     session_a = SessionPool.checkout(session_pool) |> make_old(-2*60)
     session_b = SessionPool.checkout(session_pool) |> make_old(-2*60)
@@ -57,9 +58,9 @@ defmodule Mongo.SessionTest do
   end
 
   @tag :mongo_3_6
-  test "session pool checkout prune", top do
+  test "session pool checkout prune", %{top: top} do
 
-    {:ok, session_pool} = SessionPool.start_link(top.pid, 2)
+    {:ok, session_pool} = SessionPool.start_link(top, 2)
 
     session_a = SessionPool.checkout(session_pool) |> make_old(-59)
     session_b = SessionPool.checkout(session_pool) |> make_old(-59)
@@ -78,178 +79,178 @@ defmodule Mongo.SessionTest do
   end
 
   @tag :mongo_3_6
-  test "explicit_sessions", top do
+  test "explicit_sessions", %{top: top} do
 
     coll = unique_name()
 
-    {:ok, session} = Session.start_session(top.pid, :write, [])
-    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Greta"}, session: session)
+    {:ok, session} = Session.start_session(top, :write, [])
+    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Greta"}, session: session)
     assert id != nil
-    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Waldo"}, session: session)
+    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Waldo"}, session: session)
     assert id != nil
-    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Tom"}, session: session)
+    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Tom"}, session: session)
     assert id != nil
 
-    assert %{"name" => "Tom"} = Mongo.find_one(top.pid, coll, %{name: "Tom"}, session: session)
+    assert %{"name" => "Tom"} = Mongo.find_one(top, coll, %{name: "Tom"}, session: session)
 
-    assert :ok == Session.end_session(top.pid, session)
+    assert :ok == Session.end_session(top, session)
 
   end
 
   @tag :mongo_4_2
-  test "commit_transaction", top do
+  test "commit_transaction", %{top: top} do
 
     coll = "dogs"
 
-    Mongo.insert_one(top.pid, coll, %{name: "Wuff"})
-    Mongo.delete_many(top.pid, coll, %{})
+    Mongo.insert_one(top, coll, %{name: "Wuff"})
+    Mongo.delete_many(top, coll, %{})
 
-    {:ok, session} = Session.start_session(top.pid, :write, [])
+    {:ok, session} = Session.start_session(top, :write, [])
     assert :ok = Session.start_transaction(session)
 
-    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Greta"}, session: session)
+    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Greta"}, session: session)
     assert id != nil
-    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Waldo"}, session: session)
+    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Waldo"}, session: session)
     assert id != nil
-    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Tom"}, session: session)
+    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Tom"}, session: session)
     assert id != nil
 
-    assert %{"name" => "Tom"} = Mongo.find_one(top.pid, coll, %{name: "Tom"}, session: session)
-    assert {:ok, 3} == Mongo.count_documents(top.pid, coll, %{}, session: session)
-    assert {:ok, 0} == Mongo.count_documents(top.pid, coll, %{})
+    assert %{"name" => "Tom"} = Mongo.find_one(top, coll, %{name: "Tom"}, session: session)
+    assert {:ok, 3} == Mongo.count_documents(top, coll, %{}, session: session)
+    assert {:ok, 0} == Mongo.count_documents(top, coll, %{})
 
     :ok = Session.commit_transaction(session)
 
-    %{"_id" => _id, "name" =>  "Tom"} = Mongo.find_one(top.pid, coll, %{name: "Tom"}, session: session)
-    assert {:ok, 3} == Mongo.count(top.pid, coll, %{})
+    %{"_id" => _id, "name" =>  "Tom"} = Mongo.find_one(top, coll, %{name: "Tom"}, session: session)
+    assert {:ok, 3} == Mongo.count(top, coll, %{})
 
-    assert :ok == Session.end_session(top.pid, session)
+    assert :ok == Session.end_session(top, session)
 
   end
 
   @tag :mongo_4_2
-  test "abort_transaction", top do
+  test "abort_transaction", %{top: top} do
 
     coll = "dogs"
 
-    Mongo.insert_one(top.pid, coll, %{name: "Wuff"})
-    Mongo.delete_many(top.pid, coll, %{})
+    Mongo.insert_one(top, coll, %{name: "Wuff"})
+    Mongo.delete_many(top, coll, %{})
 
-    {:ok, session} = Session.start_session(top.pid, :write, [])
+    {:ok, session} = Session.start_session(top, :write, [])
     assert :ok = Session.start_transaction(session)
 
-    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Greta"}, session: session)
+    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Greta"}, session: session)
     assert id != nil
-    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Waldo"}, session: session)
+    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Waldo"}, session: session)
     assert id != nil
-    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Tom"}, session: session)
+    {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Tom"}, session: session)
     assert id != nil
 
-    assert %{"name" => "Tom"} = Mongo.find_one(top.pid, coll, %{name: "Tom"}, session: session)
-    assert {:ok, 3} == Mongo.count_documents(top.pid, coll, %{}, session: session)
-    assert {:ok, 0} == Mongo.count_documents(top.pid, coll, %{})
+    assert %{"name" => "Tom"} = Mongo.find_one(top, coll, %{name: "Tom"}, session: session)
+    assert {:ok, 3} == Mongo.count_documents(top, coll, %{}, session: session)
+    assert {:ok, 0} == Mongo.count_documents(top, coll, %{})
 
     :ok = Session.abort_transaction(session)
 
-    assert nil == Mongo.find_one(top.pid, coll, %{name: "Tom"}, session: session)
-    assert {:ok, 0} == Mongo.count(top.pid, coll, %{})
+    assert nil == Mongo.find_one(top, coll, %{name: "Tom"}, session: session)
+    assert {:ok, 0} == Mongo.count(top, coll, %{})
 
-    assert :ok == Session.end_session(top.pid, session)
+    assert :ok == Session.end_session(top, session)
 
   end
 
   @tag :mongo_4_2
-  test "with_transaction", top do
+  test "with_transaction", %{top: top} do
 
     coll = "dogs_with_commit_transaction"
 
-    Mongo.insert_one(top.pid, coll, %{name: "Wuff"})
-    Mongo.delete_many(top.pid, coll, %{})
+    Mongo.insert_one(top, coll, %{name: "Wuff"})
+    Mongo.delete_many(top, coll, %{})
 
-    Session.with_transaction(top.pid, fn opts ->
+    Session.with_transaction(top, fn opts ->
 
-     {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Greta"}, opts)
+     {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Greta"}, opts)
       assert id != nil
-      {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Waldo"}, opts)
+      {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Waldo"}, opts)
       assert id != nil
-      {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Tom"}, opts)
+      {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Tom"}, opts)
       assert id != nil
      {:ok, :ok}
     end, w: 1)
-    assert {:ok, 3} == Mongo.count(top.pid, coll, %{})
+    assert {:ok, 3} == Mongo.count(top, coll, %{})
   end
 
   @tag :mongo_4_2
-  test "with_transaction_causal_consistency", top do
+  test "with_transaction_causal_consistency", %{top: top} do
 
     coll = "dogs_with_commit_transaction_causal_consistency"
 
-    Mongo.insert_one(top.pid, coll, %{name: "Wuff"})
-    Mongo.delete_many(top.pid, coll, %{})
+    Mongo.insert_one(top, coll, %{name: "Wuff"})
+    Mongo.delete_many(top, coll, %{})
 
-    Session.with_transaction(top.pid, fn opts ->
-     {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Greta"}, opts)
+    Session.with_transaction(top, fn opts ->
+     {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Greta"}, opts)
      assert id != nil
-     {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Waldo"}, opts)
+     {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Waldo"}, opts)
      assert id != nil
-     {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Tom"}, opts)
+     {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Tom"}, opts)
      assert id != nil
      {:ok, :ok}
     end, w: 1, causal_consistency: true)
-    assert {:ok, 3} == Mongo.count(top.pid, coll, %{})
+    assert {:ok, 3} == Mongo.count(top, coll, %{})
   end
 
   @tag :mongo_4_2
-  test "with_transaction_abort", top do
+  test "with_transaction_abort", %{top: top} do
 
     coll = "dogs_with_about_transaction"
 
-    Mongo.insert_one(top.pid, coll, %{name: "Wuff"})
-    Mongo.delete_many(top.pid, coll, %{})
+    Mongo.insert_one(top, coll, %{name: "Wuff"})
+    Mongo.delete_many(top, coll, %{})
 
-    assert :error == Session.with_transaction(top.pid, fn opts ->
+    assert :error == Session.with_transaction(top, fn opts ->
 
-      {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Greta"}, opts)
+      {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Greta"}, opts)
       assert id != nil
-      {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Waldo"}, opts)
+      {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Waldo"}, opts)
       assert id != nil
-      {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Tom"}, opts)
+      {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Tom"}, opts)
       assert id != nil
       :error
     end, w: 1)
-    assert {:ok, 0} == Mongo.count(top.pid, coll, %{})
+    assert {:ok, 0} == Mongo.count(top, coll, %{})
   end
 
   @tag :mongo_4_2
-  test "with_transaction_abort_exception", top do
+  test "with_transaction_abort_exception", %{top: top} do
 
     coll = "dogs_with_transaction_abort_exception"
 
-    Mongo.insert_one(top.pid, coll, %{name: "Wuff"})
-    Mongo.delete_many(top.pid, coll, %{})
+    Mongo.insert_one(top, coll, %{name: "Wuff"})
+    Mongo.delete_many(top, coll, %{})
 
-    assert {:error, %ArgumentError{message: "test"}} == Session.with_transaction(top.pid, fn opts ->
+    assert {:error, %ArgumentError{message: "test"}} == Session.with_transaction(top, fn opts ->
 
-     {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Greta"}, opts)
+     {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Greta"}, opts)
      assert id != nil
-     {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Waldo"}, opts)
+     {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Waldo"}, opts)
      assert id != nil
-     {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top.pid, coll, %{name: "Tom"}, opts)
+     {:ok, %InsertOneResult{:inserted_id => id}} = Mongo.insert_one(top, coll, %{name: "Tom"}, opts)
      assert id != nil
 
      raise(ArgumentError, "test")
 
     end, w: 1)
 
-    assert {:ok, 0} == Mongo.count(top.pid, coll, %{})
+    assert {:ok, 0} == Mongo.count(top, coll, %{})
   end
 
   @tag :mongo_4_2
-  test "check unordered bulk with transaction", top do
+  test "check unordered bulk with transaction", %{top: top} do
 
     coll = unique_name()
-    Mongo.insert_one(top.pid, coll, %{name: "Wuff"})
-    Mongo.delete_many(top.pid, coll, %{})
+    Mongo.insert_one(top, coll, %{name: "Wuff"})
+    Mongo.delete_many(top, coll, %{})
 
     bulk = coll
            |> UnorderedBulk.new()
@@ -263,17 +264,17 @@ defmodule Mongo.SessionTest do
            |> UnorderedBulk.delete_one(%{kind: "dog"})
            |> UnorderedBulk.delete_one(%{kind: "dog"})
 
-    {:ok, result} = Session.with_transaction(top.pid, fn opts ->
-      {:ok, BulkWrite.write(top.pid, bulk, opts)}
+    {:ok, result} = Session.with_transaction(top, fn opts ->
+      {:ok, BulkWrite.write(top, bulk, opts)}
     end, w: 1)
 
     assert %{:inserted_count => 3, :matched_count => 3, :deleted_count => 3 } ==  Map.take(result, [:inserted_count, :matched_count, :deleted_count])
-    assert {:ok, 0} == Mongo.count(top.pid, coll, %{})
+    assert {:ok, 0} == Mongo.count(top, coll, %{})
 
   end
 
   @tag :mongo_4_2
-  test "check invalid unordered bulk with transaction", top do
+  test "check invalid unordered bulk with transaction", %{top: top} do
 
     coll = unique_name()
 
@@ -286,9 +287,9 @@ defmodule Mongo.SessionTest do
            |> UnorderedBulk.update_one(%{name: "Tom"}, %{"$set": %{kind: "dog"}})
            |> UnorderedBulk.update_one(%{name: "Waldo"}, %{"$set": %{kind: "dog"}})
 
-    {:error, [result|_xs]} = Session.with_transaction(top.pid, fn opts ->
+    {:error, [result|_xs]} = Session.with_transaction(top, fn opts ->
 
-      %BulkWriteResult{errors: errors} = result = BulkWrite.write(top.pid, bulk, opts)
+      %BulkWriteResult{errors: errors} = result = BulkWrite.write(top, bulk, opts)
 
       case Enum.empty?(errors) do
          true  -> {:ok, result}
@@ -298,29 +299,53 @@ defmodule Mongo.SessionTest do
     end, w: 1)
 
     assert 263  == result["code"]
-    assert {:ok, 0} == Mongo.count(top.pid, coll, %{})
+    assert {:ok, 0} == Mongo.count(top, coll, %{})
 
   end
 
   @tag :mongo_4_2
-  test "commit empty transaction", top do
-    assert {:ok, :ok} = Session.with_transaction(top.pid, fn _opts ->
+  test "check streaming bulk with transaction", %{top: top} do
+
+    coll = unique_name()
+    Mongo.insert_one(top, coll, %{name: "Wuff"})
+    Mongo.delete_many(top, coll, %{})
+
+    assert {:ok, :ok} = Session.with_transaction(top, fn opts ->
+
+      1..1000
+      |> Stream.map(fn
+        1    -> BulkOps.get_insert_one(%{count: 1})
+        1000 -> BulkOps.get_delete_one(%{count: 999})
+        i    -> BulkOps.get_update_one(%{count: i - 1}, %{"$set": %{count: i}})
+      end)
+      |> OrderedBulk.write(top, coll, 25, opts)
+      |> Stream.run()
+
+      {:ok, :ok}
+
+    end, w: 1)
+
+  end
+
+  @tag :mongo_4_2
+  test "commit empty transaction", %{top: top} do
+    assert {:ok, :ok} = Session.with_transaction(top, fn _opts ->
      {:ok, :ok}
     end, w: 1)
   end
 
   @tag :mongo_4_2
-  test "abort empty transaction", top do
-    assert {:error, :ok} = Session.with_transaction(top.pid, fn _opts ->
+  test "abort empty transaction", %{top: top} do
+    assert {:error, :ok} = Session.with_transaction(top, fn _opts ->
       {:error, :ok}
     end, w: 1)
   end
 
   @tag :mongo_4_2
-  test "check ordered bulk with transaction", top do
+  test "check ordered bulk with transaction", %{top: top} do
     coll = unique_name()
-    Mongo.insert_one(top.pid, coll, %{name: "Wuff"})
-    Mongo.delete_many(top.pid, coll, %{})
+    Mongo.insert_one(top, coll, %{name: "Wuff"})
+    Mongo.delete_many(top, coll, %{})
 
     bulk = coll
            |> OrderedBulk.new()
@@ -335,13 +360,13 @@ defmodule Mongo.SessionTest do
            |> OrderedBulk.delete_one(%{kind: "cat"})
            |> OrderedBulk.delete_one(%{kind: "cat"})
 
-    {:ok, result} = Session.with_transaction(top.pid, fn opts ->
-      result = BulkWrite.write(top.pid, bulk, opts)
+    {:ok, result} = Session.with_transaction(top, fn opts ->
+      result = BulkWrite.write(top, bulk, opts)
       {:ok, result}
     end, w: 1)
 
     assert %{:inserted_count => 3, :matched_count => 6, :deleted_count => 3 } ==  Map.take(result, [:inserted_count, :matched_count, :deleted_count])
-    assert {:ok, 0} == Mongo.count(top.pid, coll, %{})
+    assert {:ok, 0} == Mongo.count(top, coll, %{})
 
   end
 
