b5e9cf873e7f2bda7d2f0a284c45eaf5fddadebb
Roman Pushkov <pushkovroman@me.com>
2021-05-20 16:19:42 +0200

    remove :crypto.hmac/3 from auth scram

--- lib/mongo/auth/scram.ex
+++ lib/mongo/auth/scram.ex
@@ -75,16 +75,16 @@ defmodule Mongo.Auth.SCRAM do
   end
 
   defp generate_proof(salted_password, auth_message, digest) do
-    client_key   = :crypto.hmac(digest, salted_password, "Client Key")
+    client_key   = :crypto.mac(:hmac, digest, salted_password, "Client Key")
     stored_key   = :crypto.hash(digest, client_key)
-    signature    = :crypto.hmac(digest, stored_key, auth_message)
+    signature    = :crypto.mac(:hmac, digest, stored_key, auth_message)
     client_proof = xor_keys(client_key, signature, "")
     "p=#{Base.encode64(client_proof)}"
   end
 
   defp generate_signature(salted_password, auth_message, digest) do
-    server_key = :crypto.hmac(digest, salted_password, "Server Key")
-    :crypto.hmac(digest, server_key, auth_message)
+    server_key = :crypto.mac(:hmac, digest, salted_password, "Server Key")
+    :crypto.mac(:hmac, digest, server_key, auth_message)
   end
 
   defp xor_keys("", "", result), do: result
