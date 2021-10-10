845ebe32e50506fea4ba801952c31e111c06ab04
Michael Maier <zookzook@unitybox.de>
2021-10-10 15:34:07 +0200

    fixes #109

--- test/mongo/bulk_writes_test.exs
+++ test/mongo/bulk_writes_test.exs
@@ -168,7 +168,7 @@ defmodule Mongo.BulkWritesTest do
 
   test "create one small document and one too large document", top do
     coll = unique_collection()
-    max_n = 16_777_216
+    max_n = 16*1024*1024
 
     a_line_1k = Enum.reduce(1..1_024, "", fn _, acc -> acc <> "A" end)
     a_line_1m = Enum.reduce(1..1_024, "", fn _, acc -> acc <> a_line_1k end)
