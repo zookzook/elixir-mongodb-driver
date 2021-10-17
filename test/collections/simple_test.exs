29fb8f2e3392a07842b2ad5685ee1500228634e6
Michael Maier <zookzook@unitybox.de>
2021-10-17 13:09:38 +0200

    added to simple test units

---
+++ test/collections/simple_test.exs
@@ -0,0 +1,91 @@
+defmodule Collections.SimpleTest do
+
+  use MongoTest.Case, async: false
+
+  require Logger
+
+  alias Mongo
+  alias Mongo.Collection
+
+  setup_all do
+    assert {:ok, pid} = Mongo.TestConnection.connect
+    Mongo.drop_database(pid)
+    {:ok, [pid: pid]}
+  end
+
+  defmodule Label do
+
+    use Collection
+
+    document do
+      attribute :name, String.t(), default: "warning"
+      attribute :color, String.t(), default: :red
+      after_load  &Label.after_load/1
+    end
+
+    def after_load(%Label{color: color} = label) when is_binary(color) do
+      %Label{label | color: String.to_existing_atom(color)}
+    end
+    def after_load(label)  do
+      label
+    end
+
+  end
+
+  defmodule Card do
+
+    use Collection
+
+    @collection nil
+
+    collection "cards" do
+      attribute :title, String.t(),  default: "new title"
+      attribute :created, DateString.t(), default: &DateTime.utc_now/0
+      attribute :modified, DateString.t(), default: &DateTime.utc_now/0
+      embeds_one :label, Label, default: &Label.new/0
+    end
+
+    def insert_one(%Card{} = card, top) do
+      with map <- dump(card),
+           {:ok, _} <- Mongo.insert_one(top, @collection, map) do
+        :ok
+      end
+    end
+
+    def find_one(id, top) do
+      top
+      |> Mongo.find_one(@collection, %{@id => id})
+      |> load()
+    end
+
+  end
+
+  test "load and dump", _c do
+
+    alias Collections.SimpleTest.Card
+    alias Collections.SimpleTest.Label
+
+    new_card = Card.new()
+    map_card = Card.dump(new_card)
+
+    assert %{title: "new title", label: %{color: :red, name: "warning"}} = map_card
+
+    struct_card = Card.load(map_card, true)
+
+    assert %Card{label: %Label{color: :red, name: "warning"}} = struct_card
+  end
+
+  test "save and find", c do
+
+    alias Collections.SimpleTest.Card
+    alias Collections.SimpleTest.Label
+
+    new_card = Card.new()
+
+    assert :ok = Card.insert_one(new_card, c.pid)
+
+    card = Card.find_one(new_card._id, c.pid)
+
+    assert %Card{label: %Label{color: :red, name: "warning"}} = card
+  end
+end
