4fd9d221bb7e86236502192b8b4efa3cd59fec92
zookzook <zookzook@unitybox.de>
2022-03-20 18:45:36 +0100

    feat: Use all test units

--- test/support/collection_case.ex
+++ test/support/collection_case.ex
@@ -3,7 +3,7 @@ defmodule CollectionCase do
 
   use ExUnit.CaseTemplate
 
-  @seeds ["127.0.0.1:27017", "127.0.0.1:27018", "127.0.0.1:27019"]
+  @seeds ["127.0.0.1:27017"] ## todo  , "127.0.0.1:27018", "127.0.0.1:27019"]
 
   setup_all do
     assert {:ok, pid} = Mongo.start_link(database: "mongodb_test", seeds: @seeds, show_sensitive_data_on_connection_error: true)
