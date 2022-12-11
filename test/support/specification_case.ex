db8771adf120da1090af676b7b47a3f4e2341e1e
zookzook <zookzook@unitybox.de>
2022-12-11 15:49:05 +0100

    chore: remove the mongodb version

--- test/support/specification_case.ex
+++ test/support/specification_case.ex
@@ -11,14 +11,4 @@ defmodule Mongo.SpecificationCase do
       import Mongo.SpecificationCase
     end
   end
-
-  def mongo_version do
-    {string, 0} = System.cmd("mongod", ~w'--version')
-    ["db version v" <> version, _] = String.split(string, "\n", parts: 2)
-
-    version
-    |> String.split(".")
-    |> Enum.map(&elem(Integer.parse(&1), 0))
-    |> List.to_tuple()
-  end
 end
