20183235f5de2f956ffa40c5fd0d801789298015
zookzook <zookzook@unitybox.de>
2022-12-11 15:44:31 +0100

    chore: fix test script

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,54 +1,5 @@
 # Do not run the SSL tests on Travis
-
-{string, 0} = System.cmd("mongod", ~w'--version')
-["db version v" <> version, _] = String.split(string, "\n", parts: 2)
-
-IO.puts("[mongod v#{version}]")
-
-version =
-  version
-  |> String.split(".")
-  |> Enum.map(&elem(Integer.parse(&1), 0))
-  |> List.to_tuple()
-
-options = [ssl: true, socket: true]
-
-options =
-  if System.get_env("CI") do
-    [tag_set: true] ++ options
-  else
-    options
-  end
-
-options =
-  if version < {3, 4, 0} do
-    [mongo_3_4: true] ++ options
-  else
-    options
-  end
-
-options =
-  if version < {3, 6, 0} do
-    [mongo_3_6: true] ++ options
-  else
-    options
-  end
-
-options =
-  if version < {4, 2, 0} do
-    [mongo_4_2: true] ++ options
-  else
-    options
-  end
-
-options =
-  if version < {4, 3, 0} do
-    [mongo_4_3: true] ++ options
-  else
-    options
-  end
-
-ExUnit.configure(exclude: options)
+ExUnit.configure(exclude: [ssl: true, socket: true])
 ExUnit.start()
 
 defmodule MongoTest.Case do
