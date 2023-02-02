1bd50d950b3537108b172003159e484b6cc3ed1e
ksmithut <github@ksmithut.com>
2023-02-02 11:07:46 -0700

    Forwards the :database option to the migration collection calls
    This is a way we can support dynamic multitenancy. If you pass in the :database option into the `migrate/1` function, it will forward it onto each migrations `up` function, but we also need it to use the database option for each of the calls to the migration collection, so that each tenant's migration states are kept separate.

--- test/mongo/migration_test.exs
+++ test/mongo/migration_test.exs
@@ -12,4 +12,18 @@ defmodule Mongo.MigrationTest do
     assert :unlocked == Migration.unlock()
     assert {:error, :not_locked} == Migration.unlock()
   end
+
+  test "test lock and unlock with database options", %{pid: top} do
+    Mongo.drop_collection(top, "migrations", database: "one")
+    Mongo.drop_collection(top, "migrations", database: "two")
+    Patch.patch(Mongo.Migration, :get_config, fn _ -> [topology: top, collection: "migrations", path: "migrations", otp_app: :mongodb_driver] end)
+    assert :locked == Migration.lock(database: "one")
+    assert :locked == Migration.lock(database: "two")
+    assert {:error, :already_locked} == Migration.lock(database: "one")
+    assert {:error, :already_locked} == Migration.lock(database: "two")
+    assert :unlocked == Migration.unlock(database: "one")
+    assert :unlocked == Migration.unlock(database: "two")
+    assert {:error, :not_locked} == Migration.unlock(database: "one")
+    assert {:error, :not_locked} == Migration.unlock(database: "two")
+  end
 end
